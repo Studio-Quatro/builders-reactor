a010bed8d3f498bbe70b2824fd6e71080274af1b
Juan Manuel MAffei <juanmamaffei@gmail.com>
2022-06-26 20:07:21 -0300

    feat: connection to woocommerce service

--- app/services/builders_world/connect.rb
+++ app/services/builders_world/connect.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 require 'woocommerce_api'
-
 CREDENTIALS = Rails.application.credentials.woocommerce
-
 module BuildersWorld
   # Class to connect Woocommerce shop from BuildersWorld website
+  # You could build a connection using something like these:
+  #   builders_world_website = BuildersWorld::Connect.new.call
   class Connect
     def call
-      woocommerce = WooCommerce::API.new(
+      @connector = WooCommerce::API.new(
         CREDENTIALS.domain,
         CREDENTIALS.consumer_key,
         CREDENTIALS.consumer_secret,
@@ -19,10 +19,10 @@ module BuildersWorld
       )
       # Rails.logger.debug(woocommerce)
       # woocommerce.get("products/categories").parsed_response
-      woocommerce.get('products?per_page=99&page=1').parsed_response
-      woocommerce.get('products?per_page=99&page=2').parsed_response
-      woocommerce.get('products?per_page=99&page=3').parsed_response
-      woocommerce.get('products?per_page=99&page=4').parsed_response
+      # woocommerce.get('products?per_page=99&page=1').parsed_response
+      # woocommerce.get('products?per_page=99&page=2').parsed_response
+      # woocommerce.get('products?per_page=99&page=3').parsed_response
+      # woocommerce.get('products?per_page=99&page=4').parsed_response
       # Until response == []
     end
   end
--- spec/services/builders_world/connect_spec.rb
+++ spec/services/builders_world/connect_spec.rb
@@ -4,10 +4,9 @@ require 'rails_helper'
 
 RSpec.describe(BuildersWorld::Connect, type: :service) do
   context 'when connection was succesful' do
-    it 'returns a Woocommerce object'
-  end
-
-  context 'when connection fails' do
-    it 'raises an error / returns something'
+    it 'returns a Woocommerce object' do
+      connection = described_class.new.call
+      expect(connection.instance_of?(WooCommerce::API)).to be(true)
+    end
   end
 end
