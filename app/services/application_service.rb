5b7416a216cf45c69942b6a103a1be0bc7ef1fab
Juan Manuel MAffei <juanmamaffei@gmail.com>
2022-06-26 23:28:33 -0300

    feat: application service to call in a less verbose way

--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+# Base class for services
+# rubocop : disable Style/StaticClass
 class ApplicationService
-  def self.call(*args, &block)
-    new(*args, &block).call
+  def self.call(*args, &)
+    new(*args, &).call
   end
 end
+# rubocop : enable Style/StaticClass
