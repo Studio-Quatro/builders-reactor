7204856a62b497d1fb8261b5a49bc626470e4a55
Juan Manuel MAffei <juanmamaffei@gmail.com>
2022-10-22 16:21:20 -0300

    feat: login added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV.fetch('ADMIN_USERNAME'),
+    password: ENV.fetch('ADMIN_PASSWORD')
 end
