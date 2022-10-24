df4e23d7eee57b8163e2a9843ffe320f5572738d
Juan Manuel MAffei <juanmamaffei@gmail.com>
2022-10-24 09:01:12 -0300

    feat: use case logic added

--- app/use_cases/make_list_of_changes.rb
+++ app/use_cases/make_list_of_changes.rb
@@ -2,7 +2,40 @@
 
 class MakeListOfChanges < ApplicationUseCase
   def call
+    # retrieve_data
+    make_changes_list
+    map_changes_list
+    save_changes_list
+    set_last_update
   end
 
   private
+  
+  def make_changes_list
+    @supplier_codes_to_change = ItemListComparer.call
+  end
+
+  def set_time
+    @time = Time.now
+  end
+
+  def map_changes_list
+    @changes_list = @supplier_codes_to_change.map do |supplier_code|
+      {
+        supplier_code: supplier_code,
+        status: 'pending',
+        date_of_evaluation: @time
+        created_at: @time,
+        updated_at: @time
+      }
+    end
+  end
+
+  def save_changes_list
+    ItemComparison.upsert_all(@changes_list)
+  end
+
+  def set_last_update
+    Setting.last_products_comparison = @time
+  end
 end
