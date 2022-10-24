20e5f444b4ba41b6f74ed5f37bc3238457a045f8
Juan Manuel MAffei <juanmamaffei@gmail.com>
2022-10-24 10:09:32 -0300

    feat: view added

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,6 +7,7 @@ class MainController < ApplicationController
     @last_categories_update = Setting.last_categories_update
     @last_bxa_products_update = Setting.last_bxa_products_update
     @last_bxa_categories_update = Setting.last_bxa_categories_update
+    @last_changes_list = Setting.last_products_comparison
   end
 
   # Enqueue ObtainProductsFromBwJob and respond with a message
@@ -26,4 +27,8 @@ class MainController < ApplicationController
     render json: { message: 'Product list is being built. This process could take some time... Please, do not retry the action.' }
   end
 
+  def make_list_of_changes
+    MakeListOfChangesJob.perform_later
+    render json: { message: 'Changes list is being built. This process could take some time... Please, do not retry the action.' }
+  end
 end
