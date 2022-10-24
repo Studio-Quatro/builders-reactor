2fea94fd3bd6b61752aaf6140ed822a7191363a1
Juan Manuel MAffei <juanmamaffei@gmail.com>
2022-10-24 08:41:19 -0300

    feat: items list added

---
+++ app/services/item_list_comparer.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class ItemListComparer
+  def call 
+    require_bw_items
+    require_bxa_items
+    @bw_items.each do |bw_item|
+      bxa_item = @bxa_items.find_by(supplier_code: bw_item.supplier_code)
+      unless bxa_item || compare_items(bw_item, bxa_item)
+        changes_list << bw_item.supplier_code
+      end
+    end
+    changes_list 
+  end
+
+  private
+
+  def changes_list
+    @changes_list ||= []
+  end
+
+  def compare_items(origin_item, target_item)
+    ItemComparer.new({origin_item: , target_item: }).same?
+  end
+
+  # Source
+  def require_bw_items
+    @bw_items = Item.all
+  end
+
+  # Target
+  def require_bxa_items
+    @bxa_items = Buildxact::Item.all
+  end
+end
